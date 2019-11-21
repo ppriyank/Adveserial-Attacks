3118ac04b54c07f5d11ccfb0a78d3b2785096ab7
ppriyank <priyankpathak50@gmail.com>
2019-11-21 05:34:17 -0500

    attacked audio 1

--- audio_attack1.sh
+++ audio_attack1.sh
@@ -1,8 +1,11 @@
+module load cudnn/10.0v7.6.2.24
+module load cuda/10.1.105
+
 conda activate audio
 cd /home/xl2700/p2/audio_adversarial_examples
 
 
-for file in ~/short-audio/*.wav
+for file in short-audio/*.wav
 do
   echo "$file"
   python3 classify.py --in $file --restore_path deepspeech-0.4.1-checkpoint/model.v0.4.1
