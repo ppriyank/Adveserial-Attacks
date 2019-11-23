067397b19c28dcadc042560d30ad0f469aa59c06
ppriyank <priyankpathak50@gmail.com>
2019-11-23 06:47:13 -0500

    updates

--- paper2018/main.py
+++ paper2018/main.py
@@ -1,7 +1,10 @@
 import argparse
 import numpy as np 
 import os 
+import json 
+
 from scipy.io import wavfile
+
 
 import torch
 import torch.nn as nn
@@ -14,6 +17,7 @@ from torch_stft import STFT
 
 from model import DeepSpeech, supported_rnns
 from decoder import GreedyDecoder
+from utils import reduce_tensor, check_loss
 
 
 parser = argparse.ArgumentParser(description='idk :P')
@@ -128,6 +132,87 @@ out, output_sizes = model(magnitude, input_sizes)
 decoder = GreedyDecoder(model.labels, blank_index=model.labels.index('_'))
 decoded_output, decoded_offsets = decoder.decode(out, output_sizes)
 
+transcript_path = "/scratch/pp1953/short-audio/2.txt"
+labels_map = dict([(labels[i], i) for i in range(len(labels))])
+
+labels_path = "labels.json"
+with open(labels_path) as label_file:
+            labels = str(''.join(json.load(label_file)))
+
+
+with open(transcript_path, 'r', encoding='utf8') as transcript_file:
+	transcript = transcript_file.read().replace('\n', '')
+
+
+transcript = list(filter(None, [labels_map.get(x) for x in list(transcript)]))
+out = out.transpose(0, 1)  # TxNxH
+float_out = out.float()  # ensure float32 for loss
+
+targets=[]
+targets.extend(transcript)
+targets = torch.IntTensor(targets)
+target_sizes = torch.IntTensor(batch_size)
+
+target_sizes[0]= len(transcript)
+
+
+loss = criterion(float_out, targets, output_sizes, target_sizes).to(device)
+loss = loss / magnitude.size(0)  # average the loss by minibatch
+loss = loss.to(device)
+loss_value = reduce_tensor(loss, args.world_size).item()
+            
+loss_value = loss.item()
+valid_loss, error = check_loss(loss, loss_value)
+            
+
+optimizer.zero_grad()
+# compute gradient
+
+with amp.scale_loss(loss, optimizer) as scaled_loss:
+    scaled_loss.backward()
+torch.nn.utils.clip_grad_norm_(model.parameters(), args.max_norm)
+optimizer.step()
+
+
+avg_loss += loss_value
+losses.update(loss_value, inputs.size(0))
+
+print('Epoch: [{0}][{1}/{2}]\t'
+      'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+      'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+      'Loss {loss.val:.4f} ({loss.avg:.4f})\t'.format(
+        (epoch + 1), (i + 1), len(train_sampler), batch_time=batch_time, data_time=data_time, loss=losses))
+            
+
+del loss, out, float_out
+avg_loss /= len(train_sampler)
+
+with torch.no_grad():
+    wer, cer, output_data = evaluate(test_loader=test_loader,
+                                     device=device,
+                                     model=model,
+                                     decoder=decoder,
+                                     target_decoder=decoder)
+loss_results[epoch] = avg_loss
+wer_results[epoch] = wer
+cer_results[epoch] = cer
+print('Validation Summary Epoch: [{0}]\t'
+      'Average WER {wer:.3f}\t'
+      'Average CER {cer:.3f}\t'.format(
+    epoch + 1, wer=wer, cer=cer))
+
+    values = {
+        'loss_results': loss_results,
+        'cer_results': cer_results,
+        'wer_results': wer_results
+    }
+        # anneal lr
+for g in optimizer.param_groups:
+    g['lr'] = g['lr'] / args.learning_anneal
+print('Learning rate annealed to: {lr:.6f}'.format(lr=g['lr']))
+
+best_wer = wer
+avg_loss = 0
 
 
 
